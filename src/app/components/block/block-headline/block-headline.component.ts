f315c5c7a6637556b033bf6b9d2d237b81211b75
Manuel Rauber <manuel.rauber@boundfoxstudios.com>
2024-06-03 17:47:15 +0200

    feat: add block headline

---
+++ src/app/components/block/block-headline/block-headline.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-block-headline',
+  standalone: true,
+  imports: [],
+  templateUrl: './block-headline.component.html',
+  styleUrl: './block-headline.component.css',
+})
+export class BlockHeadlineComponent {}
