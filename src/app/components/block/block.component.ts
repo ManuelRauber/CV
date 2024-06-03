3ca91b0eaf99c1202bf391050ed2eba3aebba6cb
Manuel Rauber <manuel.rauber@boundfoxstudios.com>
2024-06-03 17:43:04 +0200

    feat: add basic block component

---
+++ src/app/components/block/block.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-block',
+  standalone: true,
+  imports: [],
+  templateUrl: './block.component.html',
+  styleUrl: './block.component.css',
+})
+export class BlockComponent {}
