cb184d8ca3cfe29ed8549e4198af37162cbb4c83
Manuel Rauber <manuel.rauber@boundfoxstudios.com>
2024-06-10 19:40:58 +0200

    feat: add block-name

---
+++ src/app/components/block/block-name/block-name.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-block-name',
+  standalone: true,
+  imports: [],
+  templateUrl: './block-name.component.html',
+  styleUrl: './block-name.component.css',
+})
+export class BlockNameComponent {}
