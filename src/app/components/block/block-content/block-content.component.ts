e86d29cccd968e1bc720bfe99a080081cce6dfbd
Manuel Rauber <manuel.rauber@boundfoxstudios.com>
2024-06-10 17:31:39 +0200

    feat: block-content component

---
+++ src/app/components/block/block-content/block-content.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-block-content',
+  standalone: true,
+  imports: [],
+  templateUrl: './block-content.component.html',
+  styleUrl: './block-content.component.css',
+})
+export class BlockContentComponent {}
