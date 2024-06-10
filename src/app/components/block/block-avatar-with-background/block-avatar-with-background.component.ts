d7bf09b39ced47d063d106be4172fec739a07471
Manuel Rauber <manuel.rauber@boundfoxstudios.com>
2024-06-10 19:40:53 +0200

    feat: add block-avatar-with-background

---
+++ src/app/components/block/block-avatar-with-background/block-avatar-with-background.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-block-avatar-with-background',
+  standalone: true,
+  templateUrl: './block-avatar-with-background.component.html',
+  styleUrl: './block-avatar-with-background.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class BlockAvatarWithBackgroundComponent {
+  url = input.required();
+  alt = input.required();
+}
