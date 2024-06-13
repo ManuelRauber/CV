b5170f3b7ad08a4859d084ea296f252e02507d7c
Manuel Rauber <manuel.rauber@boundfoxstudios.com>
2024-06-13 14:59:28 +0200

    feat: add icon-text component

---
+++ src/app/components/icon-text/icon-text.component.ts
@@ -0,0 +1,17 @@
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import {
+  FaIconComponent,
+  IconDefinition,
+} from '@fortawesome/angular-fontawesome';
+
+@Component({
+  selector: 'app-icon-text',
+  standalone: true,
+  imports: [FaIconComponent],
+  templateUrl: './icon-text.component.html',
+  styleUrl: './icon-text.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class IconTextComponent {
+  icon = input.required<IconDefinition>();
+}
