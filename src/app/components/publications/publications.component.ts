11ae407c9b694817ee09592cd7c53bda6f686603
Manuel Rauber <manuel.rauber@boundfoxstudios.com>
2024-06-14 10:18:19 +0200

    feat: publications

--- src/app/components/publications/publications.component.ts
+++ src/app/components/publications/publications.component.ts
@@ -1,11 +1,46 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { BlockComponent } from '../block/block.component';
+import { BlockHeadlineComponent } from '../block/block-headline/block-headline.component';
+import { TextDividerComponent } from '../text-divider/text-divider.component';
+import { LinkComponent } from '../link/link.component';
+import { faAndroid, faApple, faWindows } from '@fortawesome/free-brands-svg-icons';
+import { faBriefcase, faCalendar, faExternalLink, faLocationPin } from '@fortawesome/free-solid-svg-icons';
+import { AvatarComponent } from '../avatar/avatar.component';
+import { CalculateDurationPipe } from '../../pipes/calculate-duration.pipe';
+import { ChipComponent } from '../chip-list/chip/chip.component';
+import { ChipListComponent } from '../chip-list/chip-list.component';
+import { ExperienceComponent } from '../experience/experience.component';
+import { IconLinkComponent } from '../icon-link-list/icon-link/icon-link.component';
+import { IconTextComponent } from '../icon-text/icon-text.component';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [
+    BlockComponent,
+    BlockHeadlineComponent,
+    TextDividerComponent,
+    LinkComponent,
+    AvatarComponent,
+    CalculateDurationPipe,
+    ChipComponent,
+    ChipListComponent,
+    ExperienceComponent,
+    IconLinkComponent,
+    IconTextComponent,
+    FaIconComponent,
+  ],
   templateUrl: './publications.component.html',
   styleUrl: './publications.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class PublicationsComponent {}
+export default class PublicationsComponent {
+  protected readonly faApple = faApple;
+  protected readonly faAndroid = faAndroid;
+  protected readonly faBriefcase = faBriefcase;
+  protected readonly faLocationPin = faLocationPin;
+  protected readonly faCalendar = faCalendar;
+  protected readonly faExternalLink = faExternalLink;
+  protected readonly faWindows = faWindows;
+}
